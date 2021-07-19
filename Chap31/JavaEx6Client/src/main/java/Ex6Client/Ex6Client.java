50156c5b1fb9da28a245e0dcaf67e674390f46a0
yoman3215 <luophilip@outlook.com>
2021-07-19 01:15:13 -0400

    Started Chap31 Ex6 Client side

---
+++ Chap31/JavaEx6Client/src/main/java/Ex6Client/Ex6Client.java
@@ -0,0 +1,25 @@
+package Ex6Client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Ex6Client extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("Ex6Client.fxml"));
+    Scene scene = new Scene(root);
+    primaryStage.setTitle("Ex6Client");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+    }
+}
---
+++ Chap31/JavaEx6Client/src/main/java/Ex6Client/Ex6ClientController.java
@@ -0,0 +1,15 @@
+ package Ex6Client;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Ex6ClientController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
